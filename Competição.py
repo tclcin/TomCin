8695a9a4129765997d9dad67820596c2d6ab4a62
tclcin <148984966+tclcin@users.noreply.github.com>
2023-10-25 16:41:17 -0300

    Delete Competição.py

--- Competição.py
+++
@@ -1,9 +0,0 @@
-diamantes_por_hora_arthur = int(input())
-diamantes_por_hora_luiz = int(input())
-diamantes_por_hora_pedro = int(input())
-tempo_horas_competicao = int(input())
-
-valor_max_diamantes = max(diamantes_por_hora_pedro, diamantes_por_hora_luiz, diamantes_por_hora_arthur) * tempo_horas_competicao
-
-print(valor_max_diamantes)
-
