e3ca7bce1c43a2f428dfe13d418738ff2596ad9d
Tome Lima <tcl@cin.ufpe.br>
2023-11-07 17:41:01 -0300

    a questão lamento resolvida e consertada
    quuestão 2012 a discutir com monito

--- ListasIP/lista-1/2012.py
+++ ListasIP/lista-1/2012.py
@@ -4,25 +4,79 @@ string_1 = input()
 string_2 = input()
 string_3 = input()
 
-letras_string_1 = len(string_1)
-letras_string_2 = len(string_2)
-letras_string_3 = len(string_3)
-
-mais_letras = 0 
-meia_letras = 0
-menos_letras = 0
-
-mais_letras = max(letras_string_1, letras_string_2, letras_string_3)
-menos_letras = min(letras_string_1, letras_string_2, letras_string_3)
-meia_letras = (letras_string_1 + letras_string_2 + letras_string_3) - (mais_letras + menos_letras)
+num_letras_string_1 = len(string_1)
+num_letras_string_2 = len(string_2)
+num_letras_string_3 = len(string_3)
 
 
+maior_string = ''
+menor_string = ''
+
+
+meia_string = ''
+
+# Definindo a maior string
+if num_letras_string_2 <= num_letras_string_1 >= num_letras_string_3:
+    maior_string = string_1
+
+elif num_letras_string_1 <= num_letras_string_2 >= num_letras_string_3:
+    maior_string = string_2
+
+else:
+    maior_string = string_3
+
+# Definindo a menor string
+if num_letras_string_2 >= num_letras_string_1 <= num_letras_string_3:
+    menor_string = string_1
+
+elif num_letras_string_1 >= num_letras_string_2 <= num_letras_string_3:
+    menor_string = string_2
+else:
+    menor_string = string_3
+
+# Definindo a string intermediária
+if (num_letras_string_2 >= num_letras_string_1 >= num_letras_string_3) or (num_letras_string_3 >= num_letras_string_1 >= num_letras_string_3):
+    meia_string = string_1
+
+elif (num_letras_string_1 >= num_letras_string_2 >= num_letras_string_2) or (num_letras_string_3 >= num_letras_string_2 >= num_letras_string_1):
+    meia_string = string_2
+
+else:
+    meia_string = string_3
+
+# Caso limite 
+if num_letras_string_1 == num_letras_string_2 == num_letras_string_3:
+    maior_string = string_1
+    menor_string = string_2
+    meia_string = string_3
 
 
 if numero_definidor == 1:
+    if len(maior_string) == len(meia_string) or len(maior_string) == len(menor_string):
+        print("(Droga! Ainda não consegui descobrir o local que possui mais sinais desconhecidos! Vou ter que ficar mais um tempo nessa Mansão Mal-Assombrada...)")
+        print("AAAAAA! Um fantasma me assustou!")
+        print('''(Uma mensagem apareceu no monitor que você estava usando. "Agente, um erro inesperado aconteceu. A EPF contactará você novamente quando tudo estiver funcionando da forma correta. Nosso sistema foi invadido por alguém que se identifica como Hubert P.Enguin")''')
 
+        if (maior_string * (menor_string < maior_string > meia_string)) + (meia_string * (menor_string < meia_string > maior_string)) + (menor_string * (meia_string < menor_string > maior_string)) != "":
+            print(f"{maior_string * (menor_string < maior_string > meia_string) + (meia_string * (menor_string < meia_string > maior_string)) + (menor_string * (meia_string < menor_string > maior_string))}")
+            print('''(Ao terminar sua tarefa, uma mensagem apareceu no monitor que você estava usando. "Muito bem agente. A EPF agradece os seus esforços")''')
     
+    else:
+        print(maior_string)
+        print('''(Ao terminar sua tarefa, uma mensagem apareceu no monitor que você estava usando. "Muito bem agente. A EPF agradece os seus esforços")''')
 
-
-
+else:
+    if len(menor_string) == len(meia_string) or len(menor_string) == len(maior_string):
+        print("(Droga! Ainda não consegui descobrir o local que possui mais sinais desconhecidos! Vou ter que ficar mais um tempo nessa Mansão Mal-Assombrada...)")
+        print("AAAAAA! Um fantasma me assustou!")
+        print('''(Uma mensagem apareceu no monitor que você estava usando. "Agente, um erro inesperado aconteceu. A EPF contactará você novamente quando tudo estiver funcionando da forma correta. Nosso sistema foi invadido por alguém que se identifica como Hubert P.Enguin")''')
+        if (maior_string * (menor_string < maior_string > meia_string)) + (meia_string * (menor_string < meia_string > maior_string)) + (menor_string * (meia_string < menor_string > maior_string)) != '':
+            print(f"{(maior_string * (menor_string < maior_string > meia_string)) + (meia_string * (menor_string < meia_string > maior_string)) + (menor_string * (meia_string < menor_string > maior_string))}")
+            print('''(Ao terminar sua tarefa, uma mensagem apareceu no monitor que você estava usando. "Muito bem agente. A EPF agradece os seus esforços")''')
     
+    else:
+        print(menor_string)
+        print('''(Ao terminar sua tarefa, uma mensagem apareceu no monitor que você estava usando. "Muito bem agente. A EPF agradece os seus esforços")''')
+
+
+print("(Depois de ler a mensagem, você dormiu. Ao acordar, você não estava mais no CIn de outubro de 2012, mas no CIn de 2023, sem acreditar na situação que vivenciou)")
--- ListasIP/lista-1/lamento.py
+++ ListasIP/lista-1/lamento.py
@@ -1,7 +1,4 @@
 numeros_validos = False
-n1 = 0
-n2 = 0
-n3 = 0 
 
 
 n1 = int(input())
@@ -18,14 +15,15 @@ if n1 > 0:
             numeros_validos = True
 
 
-if n1 == 0:
+if n1 <= 0:
     print(f"{n1} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
 
-elif n2 == 0:
+elif n2 <= 0:
     print(f"{n2} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
 
-elif n3 == 0:
+elif n3 <= 0:
     print(f"{n3} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir")
+
 
 
 if numeros_validos:
@@ -38,10 +36,12 @@ if numeros_validos:
         print(f"{palavra} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
     
 
-
+n5 = 0
 if numeros_validos and palavra_valida:
     n5 = int(input())
     
+
+if n5 > 0:
     if n1 % 2 == 0:
         n1 *= 2
     else: 
@@ -57,13 +57,19 @@ if numeros_validos and palavra_valida:
     else: 
         n3 *= 0.5
 
-
     numero_final = (n5 * n1 * n2 * n3)**(1/2)
 
     if numero_final >= 10:
-        print(f"O número {numero_final: .2f} e a palavra {palavra} eram as respostas. A caixa foi aberta.")
+        print(f"O número {numero_final:.2f} e a palavra {palavra} eram as respostas. A caixa foi aberta.")
     else:
-        print(f"A combinação era muito pequena, a caixa só vai poder ser aberta daqui a {numero_final - 10: .2f} anos.")
+        print(f"A combinação era muito pequena, a caixa só vai poder ser aberta daqui a {10-numero_final:.2f} anos.")
+
+
+
+elif numeros_validos and palavra_valida:
+    print(f"{n5} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
+
+
 
 
 
