306a00ec4848df0c89c91acae1759ba0314187d5
Tome Lima <tcl@cin.ufpe.br>
2023-11-07 10:16:24 -0300

    aparentemente errada, falarei com os monitores depois

---
+++ ListasIP/lista-1/lamento.py
@@ -0,0 +1,75 @@
+numeros_validos = False
+n1 = 0
+n2 = 0
+n3 = 0 
+
+
+n1 = int(input())
+
+if n1 > 0: 
+   
+    n2 = int(input())
+     
+    if n2 > 0:
+
+        n3 = int(input())
+
+        if n3 > 0:
+            numeros_validos = True
+
+
+if n1 == 0:
+    print(f"{n1} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
+
+elif n2 == 0:
+    print(f"{n2} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
+
+elif n3 == 0:
+    print(f"{n3} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir")
+
+if numeros_validos:
+    palavra_valida = False
+
+    palavra = input()
+    if palavra.islower():
+        palavra_valida = True
+    else:
+        print(f"{palavra} não está gravado(a) na caixa, não adianta nem continuar que ela não vai abrir.")
+    
+
+
+if numeros_validos and palavra_valida:
+    n5 = int(input())
+    
+    if n1 % 2 == 0:
+        n1 *= 2
+    else: 
+        n1 *= 0.5
+    
+    if n2 % 2 == 0:
+        n2 *= 2
+    else: 
+        n2 *= 0.5
+
+    if n3 % 2 == 0:
+        n3 *= 2
+    else: 
+        n3 *= 0.5
+
+    if n5 % 2 == 0:
+        n5 *= 2
+    else: 
+        n5 *= 0.5
+
+    numero_final = (n5 * n1 * n2 * n3)**(1/2)
+
+    if numero_final >= 10:
+        print(f"O número {numero_final: .2f} e a palavra {palavra} eram as respostas. A caixa foi aberta.")
+    else:
+        print(f"A combinação era muito pequena, a caixa só vai poder ser aberta daqui a {numero_final - 10: .2f} anos.")
+
+
+
+
+
+    
