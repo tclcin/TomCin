1242c8f2974fb01ce74ae276068e442de8c143c6
Tome Lima <tcl@cin.ufpe.br>
2023-11-07 12:47:46 -0300

    questao top3filmes completa
    questao 2012 incompleta

---
+++ ListasIP/lista-1/2012.py
@@ -0,0 +1,28 @@
+numero_definidor = int(input())
+
+string_1 = input()
+string_2 = input()
+string_3 = input()
+
+letras_string_1 = len(string_1)
+letras_string_2 = len(string_2)
+letras_string_3 = len(string_3)
+
+mais_letras = 0 
+meia_letras = 0
+menos_letras = 0
+
+mais_letras = max(letras_string_1, letras_string_2, letras_string_3)
+menos_letras = min(letras_string_1, letras_string_2, letras_string_3)
+meia_letras = (letras_string_1 + letras_string_2 + letras_string_3) - (mais_letras + menos_letras)
+
+
+
+
+if numero_definidor == 1:
+
+    
+
+
+
+    
--- ListasIP/lista-1/top3filmes.py
+++ ListasIP/lista-1/top3filmes.py
@@ -11,26 +11,84 @@ pontuacao_global_filme_3 = int(input())
 critica_filme_3 = input()
 
 
-if pontuacao_filme 
+
+if critica_filme_1 == "boa":
+    pontuacao_global_filme_1 *= 1.25
+elif critica_filme_1 == "media":
+    pontuacao_global_filme_1 *= 1
+
+elif critica_filme_1 == "ruim":
+    pontuacao_global_filme_1 *= 0.75
+
+elif critica_filme_1 == "pessima":
+    pontuacao_global_filme_1 *= 0
+
+
+
+if critica_filme_2 == "boa":
+    pontuacao_global_filme_2 *= 1.25
+elif critica_filme_2 == "media":
+    pontuacao_global_filme_2 *= 1
+
+elif critica_filme_2 == "ruim":
+    pontuacao_global_filme_2 *= 0.75
+
+elif critica_filme_2 == "pessima":
+    pontuacao_global_filme_2 *= 0
+
+
+
+if critica_filme_3 == "boa":
+    pontuacao_global_filme_3 *= 1.25
+elif critica_filme_3 == "media":
+    pontuacao_global_filme_3 *= 1
+
+elif critica_filme_3 == "ruim":
+    pontuacao_global_filme_3 *= 0.75
+
+elif critica_filme_3 == "pessima":
+    pontuacao_global_filme_3 *= 0
+
+
 
 filme_1 = ''
 filme_2 = ''
 filme_3 = ''
 zerado = False
 
-for (nome_filme, pontuacao_filme) in zip((nome_filme_1, nome_filme_2, nome_filme_3), (pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3)):
+
     
-    if pontuacao_filme == max(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
-        filme_1 = nome_filme
+if pontuacao_global_filme_1 == max(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
+    filme_1 = nome_filme_1
     
-    elif pontuacao_filme == min(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
-        filme_3 = nome_filme
+elif pontuacao_global_filme_1 == min(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
+    filme_3 = nome_filme_1
         
-        if pontuacao_filme == 0:
-            zerado = True
+else: 
+    filme_2 = nome_filme_1
+
+
+
+if pontuacao_global_filme_2 == max(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
+    filme_1 = nome_filme_2
     
-    else:
-        filme_2 = nome_filme
+elif pontuacao_global_filme_2 == min(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
+    filme_3 = nome_filme_2
+        
+else: 
+    filme_2 = nome_filme_2
+
+
+
+if pontuacao_global_filme_3 == max(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
+    filme_1 = nome_filme_3
+    
+elif pontuacao_global_filme_3 == min(pontuacao_global_filme_1, pontuacao_global_filme_2, pontuacao_global_filme_3):
+    filme_3 = nome_filme_3
+        
+else: 
+    filme_2 = nome_filme_3
+
 
 
 
@@ -41,5 +99,5 @@ print(f"{filme_1} está em 1° lugar")
 print(f"{filme_2} está em 2° lugar")
 print(f"{filme_3} está em 3° lugar")
 
-if zerado:
+if pontuacao_global_filme_1 == 0 or pontuacao_global_filme_2 == 0 or pontuacao_global_filme_3 == 0:
     print(f"{filme_3} teve uma crítica péssima")
